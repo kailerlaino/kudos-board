ce850d0ce0e3c40da063f4ce5ca9c870dd3c4d61
Kailer Laino <kailer@meta.com>
2025-06-16 16:59:05 -0700

    create sort by form

---
+++ frontend/components/SortForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./SortForm.css";
+
+function SortForm({ changeSortType }) {
+  return (
+    <form className="sort-form">
+      <select name="sort-by" onChange={(e) => changeSortType(e.target.value)}>
+        <option>Sort By</option>
+        <option value="">All</option>
+        <option value="">Recent</option>
+        <option value="">Celebration</option>
+        <option value="">Thank You</option>
+        <option value="">Inspiration</option>
+      </select>
+    </form>
+  );
+}
+
+export default SortForm;
